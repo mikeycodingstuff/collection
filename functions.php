56d48403937531b5023858761bf6e3b8f3253302
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-01 12:29:52 +0100

    unit test success case

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php
 
 function displayAlbums(array $albums) {
+    $output = '';
     foreach ($albums as $album){
-        echo '<div><p><h2>Album Title: </h2>' . $album['name'] . '</p>';
-        echo '<p><h3>Artist: </h3>' . $album['artist'] . '</p>';
-        echo '<p><h3>Number of Tracks: </h3>' . $album['tracks'] . '</p>';
-        echo '<p><h3>Album Length: </h3>' . $album['length'] . '</p></div>';
-    }
+        $output .= '<div><p><h2>Album Title: </h2>' . $album['name'] . '</p><p><h3>Artist: </h3>' . $album['artist'] . '</p><p><h3>Number of Tracks: </h3>' . $album['tracks'] . '</p><p><h3>Album Length: </h3>' . $album['length'] . '</p></div>';
+    } return $output;
 }
 
 ?>
