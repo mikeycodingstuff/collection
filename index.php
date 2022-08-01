4fa84c1b2069ec029444fae01602f119d617356a
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-01 15:26:03 +0100

    added header image and some css on the header

--- index.php
+++ index.php
@@ -10,10 +10,19 @@ $albums = fetchAlbums($db);
 <html>
     <head>
         <title>Album Collection</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti&family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet">
+        <link <link rel="stylesheet" href="normalize.css">
+        <link <link rel="stylesheet" href="styles.css">
     </head>
 
     <body>
-        <h1>Album Collection</h1>
+        <header>
+            <div>
+                <h1>Album Collection</h1>
+            </div>
+        </header>
         <main>
             <?=displayAlbums($albums);?>
         </main>
