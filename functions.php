b96a84575fa242d6fbb5ce5a6b09665b95a98aad
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-01 14:30:55 +0100

    changed get db and fetch data to functions. added wireframe plan for how things will look

--- functions.php
+++ functions.php
@@ -1,6 +1,33 @@
 <?php
+/**
+ * Gets the db and sets fetch mode to assoc
+ *
+ * @return PDO the database of albums
+ */
+function getDB(): PDO {
+    $db = new PDO('mysql:host=db; dbname=album-collection', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db; 
+}
+/**
+ * Query to fetch all the albums/data from the db
+ *
+ * @param PDO $db the db set with getDB
+ * @return array multidimensional array of albums
+ */
+function fetchAlbums(PDO $db): array {
+    $query = $db->prepare("SELECT `name`, `artist`, `tracks`, `length` FROM `albums`;");
+    $query->execute();
+    return $query->fetchAll();
+}
 
-function displayAlbums(array $albums) {
+/**
+ * Outputs album data for each album with html formatting
+ *
+ * @param array $albums array of album arrays fetched from the db
+ * @return string html formatting of data in each album array
+ */
+function displayAlbums(array $albums):string {
     $output = '';
     foreach ($albums as $album){
         if ($album === []) {
--- index.php
+++ index.php
@@ -1,36 +1,15 @@
 <?php
 
-// As a: collector
-// I want: to be able to see stored information about my collection
-// So that: I can show off to my friends
-
-// Requirements:
-
-// Items must be tied to a theme
-// Each item should have at least 3 stats about it
-// Database structure signed off by trainer before continuing
-// Information from database must be visually displayed with the stats of each item
-
-//set up pdo
-//want to pull all stats, name, artist, tracks, length
-//
-
 require_once 'functions.php';
 
-$db = new PDO('mysql:host=db; dbname=album-collection', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare("SELECT `name`, `artist`, `tracks`, `length` FROM `albums`;");
-$query->execute();
-
-$albums = $query->fetchAll();
-
-
+$db = getDB();
+$albums = fetchAlbums($db);
 
 ?>
 
 <html>
     <head>
-
+        <title>Album Collection</title>
     </head>
 
     <body>
