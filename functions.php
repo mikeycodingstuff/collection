ab7764ab9262ba5e618a8a6fafd2b71b1eb09927
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-02 15:40:21 +0100

    added preg match to validation function for length

--- functions.php
+++ functions.php
@@ -69,7 +69,10 @@ function sanitiseAndCreateArray(string $postName, string $postArtist, int $postT
  */
 function validateInputs(array $album): bool {
     if ((strlen($album['name']) <= 1000) && (strlen($album['artist']) <= 1000) && ($album['tracks'] <= 100) && (strlen($album['length']) <= 15)){
-        return true;
+        $pattern = '/[0-9]+:[0-6][0-9]/';
+        if (preg_match($pattern, $album['length'])) {
+            return true;
+        }
     } else {
         return false;
     }
