d6aa41363257f5f2b591e76d1fa2e7dfde1c464a
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-04 15:27:13 +0100

    removed html at bottom now that this page is not seen by user

--- create-album.php
+++ create-album.php
@@ -30,17 +30,3 @@ if (checkFormIsset($_POST)){
 }
 
 ?>
-
-<html>
-    <head>
-        <title>Add Album</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti&family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet">
-        <link <link rel="stylesheet" href="normalize.css">
-        <link <link rel="stylesheet" href="styles.css">
-    </head>
-
-    <body>
-    </body>
-</html>
