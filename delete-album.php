a8e1d5693bdaf734f38bc6dbf5ff32ef1aa39b3d
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-04 10:19:02 +0100

    removed else from if statement in delete-album as user is always sent to index.php

--- delete-album.php
+++ delete-album.php
@@ -7,10 +7,9 @@ $albums = fetchAlbums($db);
 
 if (isset($_POST['delete'])) {
     deleteAlbum($_POST['delete'], $db);
-    header('Location: index.php');
-}  else {
-    header('Location: index.php');
 }
+
+header('Location: index.php');
 
 ?>
 
